5fabd231f5e135d90476b6087ed762dee5b20d70
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-07-11 23:29:55 +0530

    removed sensitive keys

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,15 +1,15 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTZWVCQvG9xSzVhuw-wKBp6Aaf0_WLJMk",
-  authDomain: "prepwise-interwiev-platform.firebaseapp.com",
-  projectId: "prepwise-interwiev-platform",
-  storageBucket: "prepwise-interwiev-platform.firebasestorage.app",
-  messagingSenderId: "150716196363",
-  appId: "1:150716196363:web:220c86fc3cb22a7c50090e",
-  measurementId: "G-Y83RQVG8ME"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
